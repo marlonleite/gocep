842340c6f7cf94a6de20151d398d252945c69f8a
Marlon Leite <marlonleite@gmail.com>
2020-03-06 13:47:32 -0300

    Updated views to get params urls

--- client/views.py
+++ client/views.py
@@ -10,23 +10,28 @@ class GoCepApiView(APIView):
     """
     vc = ViaCepApi()
 
-    def get(self, request, code=None):
-        if code:
-            data = self.vc.get_by_zip_code(code)
+    def get(self, request, zipcode=None):
+        if zipcode:
+            data = self.vc.get_by_zip_code(zipcode)
         else:
-            uf = request.data.get('uf')
-            city = request.data.get('city')
-            street = request.data.get('street')
-            zip_code = request.data.get('zip_code')
 
-            if zip_code:
-                data = self.vc.get_by_zip_code(zip_code)
+            address_data = {}
+            for k, v in request.query_params.items():
+                address_data.update({k: v})
+
+            if address_data.get("zip_code"):
+                data = self.vc.get_by_zip_code(address_data["zip_code"])
             else:
-                if None in [uf, city, street]:
-                    message = "Required fields uf, city, street or zip_code"
+                if address_data.get("zip_code"):
+                    del address_data["zip_code"]
+
+                allowed_keys = {"uf", "city", "street"}
+
+                if allowed_keys != address_data.keys():
+                    message = "Required fields uf, city, street"
                     return Response({'message': message}, status.HTTP_400_BAD_REQUEST)
 
-                data = self.vc.get_by_address(uf, city, street)
+                data = self.vc.get_by_address(**address_data)
 
         if data['status'] == 200:
             return Response(data['response'], status.HTTP_200_OK)
