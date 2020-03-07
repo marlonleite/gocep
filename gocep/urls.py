41ede59adff547ef3f02e281f429b7447a151d27
Marlon Leite <marlonleite@gmail.com>
2020-03-06 23:22:56 -0300

    Better route api set in urls

--- gocep/urls.py
+++ gocep/urls.py
@@ -1,25 +1,15 @@
-"""gocep URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+    gocep URL Configuration
+"""
+from django.urls import path, include
+from rest_framework import routers
 
 from client import views
 
+router = routers.DefaultRouter()
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('address/<int:zipcode>/', views.GoCepApiView.as_view(), name='gocep-zipcode'),
-    path('address/', views.GoCepApiView.as_view(), name='gocep-address')
+    path('', include(router.urls)),
+    path('api/<int:zip_code>/', views.AddressApiView.as_view(), name='gocep-zip_code'),
+    path('api/', views.AddressApiView.as_view(), name='gocep-address')
 ]
