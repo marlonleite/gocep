403cf02322198c74e52f4436e61dc7e6c6a87a4d
Marlon Leite <marlonleite@gmail.com>
2020-03-06 23:25:11 -0300

    Updated view and created error_message to zip code bad value

--- client/views.py
+++ client/views.py
@@ -1,26 +1,45 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from .viacep import ViaCepApi
 
 
-class GoCepApiView(APIView):
+class AddressApiView(APIView):
     """
-    GoCepApiView Api to consult Address or ZipCode
+        View consult Zip Code.
     """
     vc = ViaCepApi()
 
-    def get(self, request, zipcode=None):
-        if zipcode:
-            data = self.vc.get_by_zip_code(zipcode)
-        else:
+    def error_message(self, value):
+        message_error = "Code Invalid {}"
 
+        return Response(
+            {'message': message_error.format(value)}, status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, zip_code=None):
+        """
+        Method Get address or list of addresses
+        :param zip_code: <int>
+        :param uf, city, street or zip_code: <string> query params
+        :return: dict or list addresses
+        """
+
+        if zip_code:
+            response = self.vc.get_by_zip_code(zip_code)
+
+            if response['response'].get("erro"):
+                return self.error_message(zip_code)
+        else:
             address_data = {}
             for k, v in request.query_params.items():
                 address_data.update({k: v})
 
             if address_data.get("zip_code"):
-                data = self.vc.get_by_zip_code(address_data["zip_code"])
+                response = self.vc.get_by_zip_code(address_data["zip_code"])
+
+                if response['response'].get("erro"):
+                    return self.error_message(address_data["zip_code"])
             else:
                 if address_data.get("zip_code"):
                     del address_data["zip_code"]
@@ -28,15 +47,15 @@ class GoCepApiView(APIView):
                 allowed_keys = {"uf", "city", "street"}
 
                 if allowed_keys != address_data.keys():
-                    message = "Required fields uf, city, street"
+                    message = "Required fields uf, city, street or zip_code."
                     return Response({'message': message}, status.HTTP_400_BAD_REQUEST)
 
-                data = self.vc.get_by_address(**address_data)
+                response = self.vc.get_by_address(**address_data)
 
-        if data['status'] == 200:
-            return Response(data['response'], status.HTTP_200_OK)
+        if response['status'] == 200:
+            return Response(response['response'], status.HTTP_200_OK)
 
-        if data['status'] == 400:
-            return Response({'message': data['message']}, status.HTTP_404_NOT_FOUND)
+        if response['status'] == 400:
+            return Response({'message': response['message']}, status.HTTP_404_NOT_FOUND)
 
         return Response({'error': 'API Error'}, status.HTTP_400_BAD_REQUEST)
