87e4b7ba17e396d8b58efcacc52fdffafab67d43
Marlon Leite <marlonleite@gmail.com>
2020-03-10 06:53:00 -0300

    Update ALLOWED_HOSTS settings

--- gocep/settings.py
+++ gocep/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-config('ALLOWED_HOSTS', default='127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 # Application definition
 
